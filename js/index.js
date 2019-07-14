ebb3f548180b7e29e38e5c271b3dc1369793ff43
javier <javier.castillo@studentpartner.com>
2019-07-14 09:44:30 -0500

    Primera seccion terminada

--- js/index.js
+++ js/index.js
@@ -5,7 +5,15 @@ document.querySelector(".boton").addEventListener("mouseout", () => {
     }, 400);
 });
 document.querySelector(".boton").addEventListener("mouseover", () => {
-   document.querySelector(".boton img").classList.remove("imgblanco");
+    document.querySelector(".boton img").classList.remove("imgblanco");
+});
+document.querySelector(".botonn").addEventListener("mouseout", () => {
+    setTimeout(() => {
+        document.querySelector(".botonn img").classList.remove("imgblanco");
+    }, 400);
+});
+document.querySelector(".botonn").addEventListener("mouseover", () => {
+    document.querySelector(".botonn img").classList.add("imgblanco");
 });
 
 
